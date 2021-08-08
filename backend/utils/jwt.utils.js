9c616dcd8cd76452b85776f0a9451543948827c3
benaissa-saida <benaissasaida28@gmail.com>
2021-08-08 12:02:55 +0200

    IsAdmin retiré plus utile

--- backend/utils/jwt.utils.js
+++ backend/utils/jwt.utils.js
@@ -9,7 +9,6 @@ module.exports = {
   generateTokenForUser: function(userData) {
     return jwt.sign({
       userId: userData.id,
-      isAdmin: userData.isAdmin
     },
     JWT_SIGN_SECRET,
     {
